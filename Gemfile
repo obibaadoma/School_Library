883147541cc601e973f2426d2d647ef6532bc0a6
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-10-13 00:24:09 +0300

    Fix linters

--- Gemfile
+++ Gemfile
@@ -2,5 +2,4 @@ source 'https://rubygems.org'
 gem 'minitest'
 gem 'rubocop', '>= 1.0', '< 2.0'
 
-
 # gem "rails"
--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -2,7 +2,7 @@ require_relative '../rental'
 require_relative '../teacher'
 require_relative '../book'
 require_relative '../student'
-require_relative'../classroom'
+require_relative '../classroom'
 require_relative '../person'
 
 describe Rental do
